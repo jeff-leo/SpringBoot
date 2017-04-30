862ebcbd46350f683dded97c98a317dd094bd4ec
jeff-leo <755510435@qq.com>
2017-04-30 10:26:24 +0800

    Initial commit

---
+++ src/main/java/com/liu/SpringbootApplication.java
@@ -0,0 +1,20 @@
+package com.liu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class SpringbootApplication {
+
+	@RequestMapping("/index")
+	public String index(){
+		return "Hello This is Spring Boot Test UI";
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootApplication.class, args);
+	}
+}
