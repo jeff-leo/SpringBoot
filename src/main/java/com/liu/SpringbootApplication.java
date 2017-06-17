92a83049a9f4bb7c6a4fb3185ba9719fa14ae321
jeff-leo <755510435@qq.com>
2017-06-17 16:14:32 +0800

    测试新功能1

--- src/main/java/com/liu/SpringbootApplication.java
+++ src/main/java/com/liu/SpringbootApplication.java
@@ -5,22 +5,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.servlet.http.HttpServletRequest;
-
 @SpringBootApplication
 @RestController
 public class SpringbootApplication {
 
 	@RequestMapping("/index")
-	public String index(HttpServletRequest request){
-		String serverName = request.getRequestURL().toString();
-		serverName = serverName.substring(7);
-		int end = serverName.lastIndexOf("/");
-		serverName = serverName.substring(0, end);
-		final String[] domains = serverName.split("\\.");
-		//测试新功能
-		//再测试新功能
-		return domains[0];
+	public String index(){
+		return "Hello This is Spring Boot Test UI";
 	}
 
 	public static void main(String[] args) {
