144c663ce095299bcd0bdb818eaf8ea3041afef6
jeff-leo <755510435@qq.com>
2017-06-17 16:03:48 +0800

    更新jdk版本

--- src/main/java/com/liu/SpringbootApplication.java
+++ src/main/java/com/liu/SpringbootApplication.java
@@ -5,13 +5,20 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletRequest;
+
 @SpringBootApplication
 @RestController
 public class SpringbootApplication {
 
 	@RequestMapping("/index")
-	public String index(){
-		return "Hello This is Spring Boot Test UI";
+	public String index(HttpServletRequest request){
+		String serverName = request.getRequestURL().toString();
+		serverName = serverName.substring(7);
+		int end = serverName.lastIndexOf("/");
+		serverName = serverName.substring(0, end);
+		final String[] domains = serverName.split("\\.");
+		return domains[0];
 	}
 
 	public static void main(String[] args) {
